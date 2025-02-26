1e688e5979a0efd9d22f872a71c668d6b9fc07ca
M.Ali Mousapour <zxd1385zxd1385@gmail.com>
2025-02-26 17:55:45 +0330

    set databases

--- master.py
+++ master.py
@@ -4,6 +4,28 @@ from telebot.types import InlineKeyboardButton,InlineKeyboardMarkup
 bot = telebot.TeleBot("8046345837:AAHt69dTU-Laf6faVrCVXI_NyqdPfqLi9QA")
 
 
+#creating master.db
+with sqlite3.connect('master.db') as connection:
+        cursor = connection.cursor()
+        Create_Table_categories = """
+        CREATE TABLE IF NOT EXISTS categories(
+        id integer primary key AUTOINCREMENT,    
+        c_name text,
+        c_counter integer
+         );
+        """
+        cursor.execute(Create_Table_categories)
+
+
+        Create_Table_message_ids = """
+        CREATE TABLE IF NOT EXISTS message_ids(
+        id integer primary key AUTOINCREMENT,    
+        message_ids text,
+        message_subcategory_name integer
+         );
+        """
+        cursor.execute(Create_Table_categories)
+
 
 
 
